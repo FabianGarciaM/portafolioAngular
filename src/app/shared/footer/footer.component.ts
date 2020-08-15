39e51e72d22b5daff55e09ddf818a7f97f69ac46
Fabian Garcia <marco.garcia.561@hotmail.com>
2020-08-15 00:25:50 -0500

    Template listo sin rutas

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  date:number = new Date().getFullYear();
+  namepag:string = "Fabian App";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  name:string = "Fabian App";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
